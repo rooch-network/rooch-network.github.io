b383e0e2632038262aa1b522d0b7b60f554c405b
sven <sven.tan@icloud.com>
2023-02-09 21:59:20 +0800

    support zh home page

--- src/components/HomepagePartners/index.js
+++ src/components/HomepagePartners/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
-import useBaseUrl from '@docusaurus/useBaseUrl';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 const PartnersList = [
   {
@@ -13,36 +13,49 @@ const PartnersList = [
         MoveFunsDAO has gathered more than 250 Move developers for the purpose of collective action and learning within the broader Move ecosystem, including Aptos, Starcoin, and Sui.
       </>
     ),
+    descriptionZH: (
+      <>
+        movefun DAO是一个针对Move开发者的DAO，其主要目标是团结开发者社区，建立一个跨多个Move链的生态系统。
+      </>
+    ),
   },
 ];
 
 
-function Feature({ icon, title, link, description }) {
+function Feature({ icon, title, link, description, descriptionZH }) {
+  const { siteConfig } = useDocusaurusContext();
+
+  const en = siteConfig.baseUrl === "/";
+
   return (
     <div className={clsx('col', 'col--4', styles.featureDiv)}>
       <div className={styles.cardDiv}>
-      <a href={link}>
-        <div className={styles.cardHeadDiv}>
-          {
-            icon != "" ? <img src='https://user-images.githubusercontent.com/99104408/217434466-c5670401-0094-4386-b0de-699e25f31650.png' className={styles.icon} />:<></>
-          }
-          {
-            title.indexOf("http") === 0 ?
-            <img src='https://user-images.githubusercontent.com/99104408/217434466-c5670401-0094-4386-b0de-699e25f31650.png' />:
-            <h3 className={styles.cardTitle}>{title}</h3>
-          }
-        </div>
+        <a href={link}>
+          <div className={styles.cardHeadDiv}>
+            {
+              icon != "" ? <img src={icon} className={styles.icon} /> : <></>
+            }
+            {
+              title.indexOf("http") === 0 ?
+                <img src={title} /> :
+                <h3 className={styles.cardTitle}>{title}</h3>
+            }
+          </div>
         </a>
-        <p>{description}</p>
+        <p>{en ? description : descriptionZH}</p>
       </div>
     </div>
   );
 }
 
 export default function HomepageFeatures() {
+  const { siteConfig } = useDocusaurusContext();
+
+  const en = siteConfig.baseUrl === "/";
+
   return (
     <>
-      <h1 className={styles.title}>Partnership</h1>
+      <h1 className={styles.title}>{en ? "Partnership" : "合作伙伴"}</h1>
       <section className={styles.partners}>
 
         <div className="container">
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,20 +9,25 @@ import HomepagePartners from '@site/src/components/HomepagePartners';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
+  const en = siteConfig.baseUrl === "/";
+
+  // let cJ = localStorage.getItem("sysLan");
+  // console.log(cJ);
+
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p>
-        <img src={useBaseUrl('/diagram/rooch-root-branch.svg')} className={styles.heroImage} />
+          <img src={useBaseUrl('/diagram/rooch-root-branch.svg')} className={styles.heroImage} />
         </p>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">{en ? siteConfig.tagline : "以太坊上的模块化Layer2网络，具有多链结算，由Move Language支持"}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/">
-            Start learning ⏱️
+            {en ? "Start learning" : "开始学习"} ⏱️
           </Link>
         </div>
       </div>
@@ -31,7 +36,7 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title="Home"
@@ -41,7 +46,7 @@ export default function Home() {
         <HomepageFeatures />
         <HomepagePartners />
       </main>
-    </Layout> 
+    </Layout>
   );
 }
 
