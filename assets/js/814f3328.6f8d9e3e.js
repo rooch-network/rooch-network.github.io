307d20ae2260471b9be52a8e7f690afa977d2589
jolestar <jolestar@users.noreply.github.com>
2023-02-10 07:22:40 +0000

    deploy: 1e81a8ab3e1473770bc3b8dd97bd88800f83f280

--- assets/js/814f3328.6f8d9e3e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkrooch_network=self.webpackChunkrooch_network||[]).push([[631],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Partnership Announcement~ MoveFunsDao <> Rooch","permalink":"/blog/2023/2/7/Partnership-Announcement-MoveFunsDAO"},{"title":"Hello Rooch","permalink":"/blog/2022/11/27/hello-rooch"}]}')}}]);
