e1346ba77e73f12a977e02490d83109ecf6413a9
Joe Chen <geometryolife@gmail.com>
2023-03-22 00:21:42 +0800

    add partnership in homepage

--- src/components/HomepagePartners/index.js
+++ src/components/HomepagePartners/index.js
@@ -19,6 +19,36 @@ const PartnersList = [
       </>
     ),
   },
+  {
+    title: 'https://www.movebit.xyz/images/logo-movebit.png',
+    icon: '',
+    link: 'https://github.com/movefuns',
+    description: (
+      <>
+        MoveBit is a security company for the Move ecosystem, with a vision to make the Move ecosystem the most secure Web3.<br/><br/>
+      </>
+    ),
+    descriptionZH: (
+      <>
+        MoveBit 是 Move 生态的安全公司，愿景是让 Move 生态成为最安全的 Web3。<br/><br/>
+      </>
+    ),
+  },
+  {
+    title: 'https://www.zkmove.net/assets/img/logo.jpg',
+    icon: '',
+    link: 'https://github.com/movefuns',
+    description: (
+      <>
+        A zero-knowledge Move language runtime environment. Safe, Efficient, and Privacy-protected.<br/><br/>
+      </>
+    ),
+    descriptionZH: (
+      <>
+        一个零知识的 Move 语言运行时环境。安全、高效、隐私保护。<br/><br/>
+      </>
+    ),
+  },
 ];
 
 
@@ -37,7 +67,7 @@ function Feature({ icon, title, link, description, descriptionZH }) {
             }
             {
               title.indexOf("http") === 0 ?
-                <img src={title} /> :
+                <img src={title} height="110" /> :
                 <h3 className={styles.cardTitle}>{title}</h3>
             }
           </div>
