5e54f3fd02f5608abd716d90ff351e454c40b3c9
Joe Chen <geometryolife@gmail.com>
2023-03-27 19:44:22 +0800

    add some configs

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,6 +1,9 @@
 export default {
-  logo: <span>Rooch</span>,
-  project: {
-    link: "https://github.com/shuding/nextra",
-  },
+    logo: <span>Rooch</span>,
+    project: {
+        link: 'https://github.com/rooch-network/rooch-network.github.io',
+    },
+    chat: {
+        link: 'https://discord.gg/rooch',
+    },
 };
