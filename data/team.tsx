67a74465cc29773484a575e4fe7effb6e9e50660
sven <sven.tan@icloud.com>
2023-04-13 00:49:58 +0800

    add blog filter

---
+++ data/team.tsx
@@ -0,0 +1,16 @@
+const ROOCH_TEAM: Record<string, AuthorDetails> = {
+  jolestar: {
+    name: "Jolestar",
+    twitterUsername: "jolestar",
+    picture: "/images/people/jolestar.jpg",
+  },
+};
+
+export type Author = keyof typeof ROOCH_TEAM;
+export type AuthorDetails = {
+  name: string;
+  twitterUsername?: string;
+  picture: string;
+};
+
+export default ROOCH_TEAM;
