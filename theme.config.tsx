fb4a1b33b976c237ac104448bcf44863dd247992
Mine77 <haichaozhu77@gmail.com>
2023-04-15 00:06:56 +0800

    a messy commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,30 +1,29 @@
-
-import { DocsThemeConfig } from "nextra-theme-docs"
-import { Footer } from "./components/footer"
+import { DocsThemeConfig } from "nextra-theme-docs";
+import { Footer } from "./components/footer";
 
 const theme: DocsThemeConfig = {
-    logo: <span>Rooch</span>,
-    useNextSeoProps() {
-        return {
-            titleTemplate: '%s – Rooch'
-        }
-    },
-    project: {
-        link: 'https://github.com/rooch-network/rooch-network.github.io',
-    },
-    chat: {
-        link: 'https://discord.gg/rooch',
-    },
-    i18n: [
-        { locale: 'en-US', text: 'English' },
-        { locale: 'zh-CN', text: '简体中文' },
-    ],
-    footer: {
-        component: Footer,
-    },
-    sidebar: {
-        defaultMenuCollapseLevel: 1
-    }
-}
+  logo: <span>Rooch</span>,
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – Rooch",
+    };
+  },
+  project: {
+    link: "https://github.com/rooch-network/rooch-network.github.io",
+  },
+  chat: {
+    link: "https://discord.gg/rooch",
+  },
+  i18n: [
+    { locale: "en-US", text: "English" },
+    { locale: "zh-CN", text: "简体中文" },
+  ],
+  footer: {
+    component: Footer,
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+  },
+};
 
-export default theme
+export default theme;
