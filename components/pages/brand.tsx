0c8e895545506d0f6204fb8eb83fb3207d61f1ab
Joe Chen <geometryolife@gmail.com>
2023-05-22 23:03:39 +0800

    update branding download

--- components/pages/brand.tsx
+++ components/pages/brand.tsx
@@ -13,17 +13,10 @@ const Brand: FC<DownloadButtonProps> = ({ images }) => {
   };
 
   console.log(images);
+  console.log(images[0]);
 
   return (
     <>
-      <p className="text-2xl mt-8">Assets</p>
-      <button
-        className="w-64 rounded-3xl mt-6 p-2 border hover:border-black hover:bg-black hover:text-white"
-        onClick={() => download("/rooch.zip")}
-      >
-        DOWNLOAD ASSETS (.ZIP FILE)
-      </button>
-
       {images.map((url, index) => (
         <div key={url} className="flex flex-row border-t-2 mt-6">
           <div className="basis-1/2 mt-6">
@@ -31,13 +24,13 @@ const Brand: FC<DownloadButtonProps> = ({ images }) => {
             <div className="flex flex-row mt-6">
               <button
                 className="basis-1/3 rounded-3xl p-2 border hover:border-black hover:bg-black hover:text-white"
-                onClick={() => download(url)}
+                onClick={() => download(images[0])}
               >
                 PNG
               </button>
               <button
                 className="basis-1/3 ml-8 rounded-3xl p-2 border hover:border-black hover:bg-black hover:text-white"
-                onClick={() => download(url)}
+                onClick={() => download(images[1])}
               >
                 SVG
               </button>
